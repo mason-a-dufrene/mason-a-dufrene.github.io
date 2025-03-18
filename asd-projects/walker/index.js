a1c00f11ffb006f8a00bc4aa9522245030839f5b
mason-dufrene <141771148+mason-dufrene@users.noreply.github.com>
2025-03-18 16:59:13 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -12,12 +12,23 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+var KEY = {
+  ENTER : 13,
+  RIGHT : 39,
+  LEFT : 37,
+  UP : 38,
+  DOWN : 40,
+}
 
+var posititionX = 0   // decides x positition 
+var posititionY = 0 //determines the Y positition
+var speedX = 0 //determines speed alonng the X axis
+var speedY = 0 //determines the speed along the Y-axis
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  var interval = setInterval(newFrame, 60000);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('keydown', handleKeyDown);        // change 'eventType' to the type of event you want to handle
+  
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -26,17 +37,34 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-  function newFrame() {
-    
+  function newFrame(event) {
+    function reposititionWalker(){
+posititionX += speedX
+posititionY += speedY
+    }
 
+function redrawWalker(){
+  $("#walker").css("left", posititionX)
+  $("#walker").css("top", posititionY)
   }
-  
+}
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  // CORE LOGIC...
+function handleKeyDown(event) {
+  if(event.which === KEY.ENTER){
+    walker.speedX = 5
+  }if(event.which === KEY.LEFT){
+    walker.speedX = -5
+  }if(event.which === KEY.RIGHT){
+    console.log("Right pressed")
+  }if(event.which === KEY.UP){
+    walker.speedY = 5
+  }if(event.which === KEY.DOWN){
+    walker.speedY = -5
   }
+}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
