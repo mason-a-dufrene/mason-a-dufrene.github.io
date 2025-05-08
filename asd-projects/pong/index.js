a02c1e3c7fdc61fb6e5d8e5e4f60a957d29912f0
mason-a-dufrene <mad.27mason@gmail.com>
2025-05-08 02:12:32 +0000

    saving Pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,11 +12,31 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+  function gameItem(box){
+  box = {};
+  box.x = parseFloat$("#" + box).css("left");
+  box.y = parseFloat$("#" + box).css("top");
+  box.width = $("#" + box).width();
+  box.height = $("#" + box).height();
+  box.speedX = 1;
+  box.speedY = 1;
+  box.id = "#" + box;
+  }
+gameItem(ball)
+gameItem(rightPaddle)
+gameItem(leftPaddle)
 
+var KEY = {
+  UP: 38,
+  RIGHT: 39,
+  W: 87,
+  S: 83,
+}
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  startBall()
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -28,8 +48,8 @@ function runProgram(){
   */
   function newFrame() {
     function moveBox() {
-      box.x += box.speedX; // update the position of the box along the x-axis
-      $(box.id).css("left", box.x); // draw the box in the new location, positionX pixels away from the "left"
+      ball.x += ball.speedX; // update the position of the box along the x-axis
+      $(ball.id).css("left", ball.x); // draw the box in the new location, positionX pixels away from the "left"
     }
 
   }
@@ -37,21 +57,54 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+ 
+  function handleKeyDown(event) {
+var KEY = event.whitch
+console.log(KEY)
+if (KEY === KEY.S){
+  console.log("s pressed")
+}
 
+    if (event.which === KEY.UP) {
+      rightPaddle.speedY = -5;
+
+    }if (event.which === KEY.DOWN) {
+      rightPaddle.speedY = 5;
+
+
+    }if (event.which === KEY.W) {
+      leftPaddle.speedY = -5;
+
+      
+    }if (event.which === KEY.S) {
+      walker.speedY = 5;
+
+    }
+  }
+
+  function handleKeyUp(event){
+    if(event.which === KEY.UP) {
+      rightPaddle.speedY = 0;
+
+    }if(event.which === KEY.DOWN) {
+      rightPaddle.speedY = 0;
+
+    }if(event.which === KEY.W) {
+      leftPaddle.speedY = 0;
+
+    }if(event.which === KEY.S) {
+      leftPaddle.speedY = 0;
+
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  var box = {};
-  box.x = 0;
-  box.y = 100;
-  box.width = 200;
-  box.height = 200;
-  box.speedX = 1;
-  box.speedY = 1;
-  box.id = "#box";
+function startBall(){
+  ball.x = 200;
+  ball.y = 200;
+}
   
   function endGame() {
     // stop the interval timer
