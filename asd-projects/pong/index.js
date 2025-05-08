239b24730dafa3c3fd58d543b8bf862b2fd1c6a1
mason-a-dufrene <mad.27mason@gmail.com>
2025-05-08 16:54:54 +0000

    adjustments

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,31 +12,29 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-  function gameItem(box){
+  function MakeGameItem(box){
   box = {};
-  box.x = parseFloat$("#" + box).css("left");
-  box.y = parseFloat$("#" + box).css("top");
-  box.width = $("#" + box).width();
-  box.height = $("#" + box).height();
+  box.x = $(box).css("left");
+  box.y = $(box).css("top");
+  box.width = $(box).width();
+  box.height = $(box).height();
   box.speedX = 1;
   box.speedY = 1;
-  box.id = "#" + box;
+  box.id = box;
+  return box
   }
-gameItem(ball)
-gameItem(rightPaddle)
-gameItem(leftPaddle)
+  MakeGameItem("#ball")
+  MakeGameItem("#rightPaddle")
+  MakeGameItem("#leftPaddle")
 
-var KEY = {
-  UP: 38,
-  RIGHT: 39,
-  W: 87,
-  S: 83,
-}
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);            // handle key down
+  $(document).on('keyup', handleKeyUp); 
   startBall()
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -47,56 +45,67 @@ var KEY = {
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    function moveBox() {
-      ball.x += ball.speedX; // update the position of the box along the x-axis
-      $(ball.id).css("left", ball.x); // draw the box in the new location, positionX pixels away from the "left"
-    }
+  moveBox()
 
   }
-  
   /* 
   Called in response to events.
   */
- 
+  var KEYCODE = {
+    UP: 38,
+    DOWN: 40,
+    W: 87,
+    S: 83,
+  }
+
   function handleKeyDown(event) {
-var KEY = event.whitch
-console.log(KEY)
-if (KEY === KEY.S){
+var keycode = event.which
+console.log(keycode)
+
+if (keycode === KEYCODE.S){
   console.log("s pressed")
 }
+if (keycode === KEYCODE.W){
+  console.log("w pressed")
+}
+if (keycode === KEYCODE.UP){
+  console.log("up pressed")
+}
+if (keycode === KEYCODE.DOWN){
+  console.log("down pressed")
+}
+    // if (event.which === KEY.UP) {
+    //   rightPaddle.speedY = -5;
 
-    if (event.which === KEY.UP) {
-      rightPaddle.speedY = -5;
-
-    }if (event.which === KEY.DOWN) {
-      rightPaddle.speedY = 5;
+    // }if (event.which === KEY.DOWN) {
+    //   rightPaddle.speedY = 5;
 
 
-    }if (event.which === KEY.W) {
-      leftPaddle.speedY = -5;
+    // }if (event.which === KEY.W) {
+    //   leftPaddle.speedY = -5;
 
       
-    }if (event.which === KEY.S) {
-      walker.speedY = 5;
+    // }if (event.which === KEY.S) {
+    //   leftPaddle.speedY = 5;
 
-    }
+    // }
   }
 
-  function handleKeyUp(event){
-    if(event.which === KEY.UP) {
-      rightPaddle.speedY = 0;
+  // function handleKeyUp(event){
+  //   if(event.which === KEY.UP) {
+  //     rightPaddle.speedY = 0;
 
-    }if(event.which === KEY.DOWN) {
-      rightPaddle.speedY = 0;
+  //   }if(event.which === KEY.DOWN) {
+  //     rightPaddle.speedY = 0;
 
-    }if(event.which === KEY.W) {
-      leftPaddle.speedY = 0;
+  //   }if(event.which === KEY.W) {
+  //     leftPaddle.speedY = 0;
 
-    }if(event.which === KEY.S) {
-      leftPaddle.speedY = 0;
+  //   }if(event.which === KEY.S) {
+  //     leftPaddle.speedY = 0;
 
-    }
-  }
+  //   }
+  // }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -104,6 +113,10 @@ if (KEY === KEY.S){
 function startBall(){
   ball.x = 200;
   ball.y = 200;
+}
+function moveBox() {
+  ball.x += ball.speedX; // update the position of the box along the x-axis
+  $(ball.id).css("left", ball.x); // draw the box in the new location, positionX pixels away from the "left"
 }
   
   function endGame() {
